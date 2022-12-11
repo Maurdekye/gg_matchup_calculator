f538e35fbd333e05b75e963ea60b6c10223dda58
Maurdekye <ncolaprete@gmail.com>
2022-12-11 10:14:03 -0500

    added print statement showing result

--- matchup_calc.py
+++ matchup_calc.py
@@ -12,6 +12,7 @@ def main(args):
     # load data
     with args.file.open(newline='') as file:
         data = {r['VS'] : {c: float(m)/100 for c, m in r.items() if c not in ['VS', r['VS']]} for r in DictReader(file)}
+    print(f"Loaded matchup data from {args.file.name}")
 
     # do the algorithm :p
     scores, grand_mult, _, iters, hit_max_iters = next(dropwhile(lambda params: params[2] >= args.max_settle and not params[4],accumulate(count(1),lambda params, it: ((lambda scores, grand_mult: ((lambda new_scores: ((lambda new_grand_mult: (new_scores,new_grand_mult,abs(grand_mult - new_grand_mult),it,it >= args.max_iters,))(1 / sum(abs(s) for s in new_scores.values()), )))({c: sum((2*s - 1) * 2**scores[o] * grand_mult for o, s in data[c].items()) for c in data.keys()},)))(params[0],params[1],)),initial = ({c: 0 for c in data.keys()}, 1, args.max_settle+1, 0, False))))
@@ -20,8 +21,7 @@ def main(args):
     if hit_max_iters:
         print(f"Did not settle after {iters} iterations! data must be weird owO")
     
-    print(f"""
-Took {iters} iterations to settle to a grand multiplier of {grand_mult}.
+    print(f"""Took {iters} iterations to settle to a grand multiplier of {grand_mult}.
 These are the individual ranking scores:
 """ + '\n'.join(f'{i+1}. {c}: {s}' for i, (c, s) in enumerate(sorted(scores.items(), key=lambda x:x[1], reverse=True))))
 
