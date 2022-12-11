4e82a7c0c36367c16419f70df8659446a1fec006
Maurdekye <ncolaprete@gmail.com>
2022-12-11 08:13:07 -0500

    add main

--- matchup_calc.py
+++ matchup_calc.py
@@ -3,3 +3,9 @@ from pathlib import Path
 
 parser = ArgumentParser("Calculate character rankings based on matchup data")
 parser.add_argument("file", type=Path, required=True, help="CSV file containing character matchup data")
+
+def main(args):
+    pass
+
+if __name__ == "__main__":
+    main(parser.parse_args())
