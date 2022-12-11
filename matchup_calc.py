90d2f85ab2f1c064751f24f615d87845c3aa123d
Maurdekye <ncolaprete@gmail.com>
2022-12-11 09:07:27 -0500

    added max iters arg

--- matchup_calc.py
+++ matchup_calc.py
@@ -7,6 +7,7 @@ from functools import *
 parser = ArgumentParser("Calculate character rankings based on matchup data")
 parser.add_argument("file", type=Path, help="CSV file containing character matchup data")
 parser.add_argument("--max-settle", type=float, default=0.000001, help="Wait unil the final scores settle this much before giving a final answer")
+parser.add_argument("--max-iters", type=int, default=1000, help="Max number of iterations before manually exiting")
 
 def main(args):
     with args.file.open(newline='') as file:
@@ -15,6 +16,7 @@ def main(args):
     scores = {c: 0 for c in data.keys()}
     grand_mult = 1
     iters = 0
+    hit_max_iters = False
     while True:
         mults = {c: 2**s for c, s in scores.items()}
         scores = {c: sum((2*s - 1) * mults[o] * grand_mult for o, s in data[c].items()) for c in data.keys()}
@@ -25,6 +27,12 @@ def main(args):
         iters += 1
         if mult_diff < args.max_settle:
             break
+        if iters >= args.max_iters:
+            hit_max_iters = True
+            break
+    
+    if hit_max_iters:
+        print(f"Did not settle after {iters} iterations! data must be weird owO")
     
     print(f"""Took {iters} iterations to settle to a grand multiplier of {grand_mult}.
 These are the individual ranking scores:""")
